1d0c0c9a4d5497b386bcb19bff48d09b74427c28
codingdefined <codingdefined@gmail.com>
2018-06-28 16:07:55 +0530

    Added Initial Migration and the Top Bar Component Ignoring the Log File using .gitignore

---
+++ src/LetsDisc.Web.Mvc/Views/Shared/Components/TopBar/TopBarViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Abp.Application.Navigation;
+using Abp.Localization;
+using LetsDisc.Sessions.Dto;
+
+namespace LetsDisc.Web.Views.Shared.Components.TopBarLanguageSwitch
+{
+    public class TopBarViewModel
+    {
+        public GetCurrentLoginInformationsOutput LoginInformations { get; set; }
+
+        public UserMenu MainMenu { get; set; }
+
+        public string ActiveMenuItemName { get; set; }
+
+        public bool IsMultiTenancyEnabled { get; set; }
+
+        public string GetShownLoginName()
+        {
+            if(LoginInformations != null && LoginInformations.User != null)
+            {
+                var userName = "<a id=\"HeaderCurrentUserName\">" + LoginInformations.User.UserName + "</a>";
+
+                if (!IsMultiTenancyEnabled)
+                {
+                    return userName;
+                }
+
+                return LoginInformations.Tenant == null
+                    ? ".\\" + userName
+                    : LoginInformations.Tenant.TenancyName + "\\" + userName;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
