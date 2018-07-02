4a2f74927a6c1afc11e9546c2bfd6fbd005db8f6
codingdefined <codingdefined@gmail.com>
2018-07-02 16:13:34 +0530

    Added Vote feature in Question (Upvote and Downvote)

---
+++ src/LetsDisc.Web.Mvc/wwwroot/view-resources/Views/Questions/Details.js
@@ -0,0 +1,35 @@
+﻿(function() {
+    $(function() {
+
+        var _questionService = abp.services.app.question;
+
+        $('.vote-up').click(function () {
+            var questionId = $(this).attr("data-question-id");
+            upVote(questionId);
+        });
+
+        $('.vote-down').click(function () {
+            var questionId = $(this).attr("data-question-id");
+            downVote(questionId);
+        });
+
+
+        function upVote(questionId) {
+            _questionService.questionVoteUp({
+                id: questionId
+            }).done(function (data) {
+                $('.upvote-count').text(data.voteCount);
+                abp.notify.info("Saved your vote. Thanks.");
+            });
+        }
+
+        function downVote(questionId) {
+            _questionService.questionVoteDown({
+                id: questionId
+            }).done(function (data) {
+                $('.upvote-count').text(data.voteCount);
+                abp.notify.info("Saved your vote. Thanks.");
+            });
+        }
+    });
+})();
